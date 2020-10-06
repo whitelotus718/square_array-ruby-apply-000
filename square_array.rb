8063f583c40a34e22575e0ceac72c0a3e19b19e0
Minhaj Khan <minhajaswe@gmail.com>
2020-10-06 08:31:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,8 @@
 def square_array(array)
+  new_arr = []
+
   array.each do |ele| 
-  i = 0
-  while i <= array.length
-    arr = []
-    arr.push(ele ** 2)
-    i += 1
+    new_arr << (ele ** 2)
   end
-    return arr
-  end
+  
 end
